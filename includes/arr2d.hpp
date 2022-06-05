bfd5a2156a0a4f498619ed3d451b461f95d1f964
Nicholas Lukasevich <n.luka784@gmail.com>
2022-06-04 21:05:22 -0400

    `arr2d`: add `cmp`, `is_homogenous`

--- includes/arr2d.hpp
+++ includes/arr2d.hpp
@@ -32,6 +32,42 @@ ElemT max(
   return max;
 }
 
+template<typename ElemT>
+constexpr
+bool cmp(
+  ElemT const *const arr1,
+  ElemT const *const arr2,
+  size_t const width,
+  size_t const height,
+  size_t const startIdx = 0
+) {
+  size_t const len = width * height;
+  for (size_t i = startIdx; i < len; ++i) {
+    if (arr1[i] != arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+template<typename ElemT>
+constexpr
+bool is_homogenous(
+  ElemT const *const arr,
+  size_t const width,
+  size_t const height,
+  size_t const startIdx = 0
+) {
+  size_t const len = width * height;
+  ElemT const &firstElem = arr[startIdx];
+  for (size_t i = startIdx + 1; i < len; ++i) {
+    if (arr[i] != firstElem) {
+      return false;
+    }
+  }
+  return true;
+}
+
 } // namespace arr2d
 
 #endif // CPPLIB_ARR2D_HPP
