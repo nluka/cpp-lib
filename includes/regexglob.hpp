a5aad04d86256390ee879f2224304c3585c2d1b9
Nicholas Lukasevich <n.luka784@gmail.com>
2022-06-21 20:01:49 -0400

    `regexglob`: fix tests on linux, add `homogenize_path_separators`

--- includes/regexglob.hpp
+++ includes/regexglob.hpp
@@ -11,6 +11,9 @@ namespace regexglob {
 
 void set_preferred_separator(char);
 
+void homogenize_path_separators(std::filesystem::path &path, char const sep);
+void homogenize_path_separators(std::string &path, char const sep);
+
 #if REGEXGLOB_LOGGING_ENABLED
 void set_ofstream(std::ofstream *);
 #endif // REGEXGLOB_LOGGING_ENABLED
