f9c7f92990bc6ce33213f447c852cec0ddc3df1d
Nicholas Lukasevich <n.luka784@gmail.com>
2022-07-10 02:32:22 -0400

    `scoped-timer`: fix for Linux compilation

--- includes/scoped-timer.hpp
+++ includes/scoped-timer.hpp
@@ -19,26 +19,37 @@ public:
   ScopedTimer(char const *const label, std::ostream &os)
   : m_label{label},
     m_os{os},
-    m_start{std::chrono::high_resolution_clock::now()}
+    m_start{std::chrono::steady_clock::now()}
   {}
 
   ~ScopedTimer() {
-    auto const end = std::chrono::high_resolution_clock::now();
+    auto const end = std::chrono::steady_clock::now();
 
-    std::chrono::duration<
-      double,
-      std::ratio<1, static_cast<intmax_t>(UnitT)>
-    > const elapsedTime = end - m_start;
+    auto const elapsedNanos = end - m_start;
 
-    m_os << m_label << " took " << elapsedTime.count() << ([](){
+    auto const elapsedTimeInTemplatedUnits =
+      static_cast<double>(elapsedNanos.count()) /
+      static_cast<double>(([](){
+        switch (UnitT) {
+          default:
+          case TimerUnit::SECONDS:      return TimerUnit::NANOSECONDS;
+          case TimerUnit::MILLISECONDS: return TimerUnit::MICROSECONDS;
+          case TimerUnit::MICROSECONDS: return TimerUnit::MILLISECONDS;
+          case TimerUnit::NANOSECONDS:  return TimerUnit::SECONDS;
+        }
+      })());
+
+    char const *const units = ([](){
       switch (UnitT) {
+        default:
         case TimerUnit::SECONDS:      return "s";
         case TimerUnit::MILLISECONDS: return "ms";
         case TimerUnit::MICROSECONDS: return "us";
         case TimerUnit::NANOSECONDS:  return "ns";
-        default:                      return "units";
       }
-    })() << '\n';
+    })();
+
+    m_os << m_label << " took " << elapsedTimeInTemplatedUnits << units << '\n';
   }
 
 private:
